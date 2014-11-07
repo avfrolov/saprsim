d4a80b6b7ae68640bc9027aa9f09427fc3c1d9b0
slimden <senchenko.d@gmail.com>
2014-11-07 17:58:46 +0400

    Model class. Contains all schema entities.

---
+++ Entity/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    class Model
+    {
+        List<Entity> entities = new List<Entity>();
+
+        public void addEntity(Entity entity)
+        {
+            entities.Add(entity);
+        }
+
+        public List<Entity> getEntities()
+        {
+            return entities;
+        }
+    }
+}
