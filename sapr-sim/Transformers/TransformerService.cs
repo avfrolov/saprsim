9616c170eaa30ed935f4a6b255f15cb17ed3f29f
avfrolov <avfrolov9302@gmail.com>
2015-03-28 22:12:38 +0300

    TransformerService refactoring

--- sapr-sim/Transformers/TransformerService.cs
+++ sapr-sim/Transformers/TransformerService.cs
@@ -38,85 +38,12 @@ namespace EntityTransformator
             foreach (UIElement e in elements)
             {
                 if (e is ConnectionLine)
-                {
-                    ConnectionLine c = e as ConnectionLine;
-                    UIEntity src = c.SourcePort.Owner;
-                    UIEntity dest = c.DestinationPort.Owner;
-
-                    if (src is SubDiagram || dest is SubDiagram) continue; 
-
-                    if (map.ContainsKey(src) && map.ContainsKey(dest))
-                    {
-                        Entity realSrc = map[src];
-                        Entity realDest = map[dest];
-                        
-                        if (c.SourcePort.PortType == PortType.OUTPUT && c.DestinationPort.PortType == PortType.INPUT)
-                        {
-                            realSrc.addOutput(realDest);
-                            realDest.addInput(realSrc);
-                        }
-                        else if (c.SourcePort.PortType == PortType.INPUT && c.DestinationPort.PortType == PortType.OUTPUT)
-                        {
-                            realSrc.addInput(realDest);
-                            realDest.addOutput(realSrc);
-                        }
-                    }
-                }
+                    processConnectionLine(e as ConnectionLine, true);
                 else if (e is sapr_sim.Figures.Resource)
-                {
-                    sapr_sim.Figures.Resource resource = e as sapr_sim.Figures.Resource;
-                    List<ConnectionLine> connectors = ConnectorFinder.find(elements, resource);
-
-                    Entities.Resource res = new Entities.Resource() { efficiency = resource.Efficiency , price = resource.Price, count = resource.Count ,isShared = resource.IsShared};
-                    resources.Add(res);
-
-                    if (connectors.Count > 0)
-                    {
-                        UIEntity procedure = null;
-                        if (connectors[0].SourcePort != null && connectors[0].SourcePort.PortType == PortType.RESOURCE)
-                        {
-                            UIEntity src = connectors[0].SourcePort.Owner;
-                            UIEntity dst = connectors[0].DestinationPort.Owner;
-                            procedure = src is Procedure ? src : dst is Procedure ? dst : null;
-                            if (procedure != null)
-                            {
-                                addAdditionalRelations(map[procedure], res);
-                            }                                
-                        }                         
-                    }
-                }
+                    processResource(elements, e as sapr_sim.Figures.Resource);
             }
 
-            foreach (UIElement e in elements)
-            {
-                if (e is SubDiagram)
-                {
-                    SubDiagram sd = e as SubDiagram;
-                    List<ConnectionLine> connectors = ConnectorFinder.find(elements, sd);
-                    foreach (ConnectionLine cl in connectors)
-                    {
-                        UIEntity src = cl.SourcePort.Owner;
-                        UIEntity dest = cl.DestinationPort.Owner;
-
-                        if (map.ContainsKey(src) && map.ContainsKey(dest))
-                        {
-                            Entity realSrc = map[src];
-                            Entity realDest = map[dest];
-
-                            if (cl.SourcePort.PortType == PortType.OUTPUT && cl.DestinationPort.PortType == PortType.INPUT)
-                            {
-                                realSrc.addOutput(realDest);
-                                realDest.addInput(realSrc);
-                            }
-                            else if (cl.SourcePort.PortType == PortType.INPUT && cl.DestinationPort.PortType == PortType.OUTPUT)
-                            {
-                                realSrc.addInput(realDest);
-                                realDest.addOutput(realSrc);
-                            }
-                        }
-                    }
-                }
-            }
+            processSubprocess(elements);
 
             return realEntities;
         }
@@ -142,7 +69,69 @@ namespace EntityTransformator
             return resources;
         }
 
-        // TODO create additional map service
+        private void processConnectionLine(ConnectionLine c, bool skipSubprocess)
+        {
+            UIEntity src = c.SourcePort.Owner;
+            UIEntity dest = c.DestinationPort.Owner;
+
+            if (skipSubprocess && (src is SubDiagram || dest is SubDiagram)) return;
+
+            if (map.ContainsKey(src) && map.ContainsKey(dest))
+                addLinkForRealEntities(c, map[src], map[dest]);
+        }
+
+        private void processResource(UIElementCollection elements, sapr_sim.Figures.Resource resource)
+        {
+            List<ConnectionLine> connectors = ConnectorFinder.find(elements, resource);
+
+            Entities.Resource res = new Entities.Resource() { efficiency = resource.Efficiency, price = resource.Price, count = resource.Count, isShared = resource.IsShared };
+            resources.Add(res);
+
+            if (connectors.Count > 0)
+            {
+                UIEntity procedure = null;
+                if (connectors[0].SourcePort != null && connectors[0].SourcePort.PortType == PortType.RESOURCE)
+                {
+                    UIEntity src = connectors[0].SourcePort.Owner;
+                    UIEntity dst = connectors[0].DestinationPort.Owner;
+                    procedure = src is Procedure ? src : dst is Procedure ? dst : null;
+
+                    if (procedure != null)
+                        addAdditionalRelations(map[procedure], res);
+                }
+            }
+        }
+
+        private void processSubprocess(UIElementCollection elements)
+        {
+            foreach (UIElement e in elements)
+            {
+                if (e is SubDiagram)
+                {
+                    SubDiagram sd = e as SubDiagram;
+                    List<ConnectionLine> connectors = ConnectorFinder.find(elements, sd);
+                    foreach (ConnectionLine cl in connectors)
+                    {
+                        processConnectionLine(cl, false);
+                    }
+                }
+            }
+        }
+
+        private void addLinkForRealEntities(ConnectionLine c, Entity realSrc, Entity realDest)
+        {
+            if (c.SourcePort.PortType == PortType.OUTPUT && c.DestinationPort.PortType == PortType.INPUT)
+            {
+                realSrc.addOutput(realDest);
+                realDest.addInput(realSrc);
+            }
+            else if (c.SourcePort.PortType == PortType.INPUT && c.DestinationPort.PortType == PortType.OUTPUT)
+            {
+                realSrc.addInput(realDest);
+                realDest.addOutput(realSrc);
+            }
+        }
+
         private void addAdditionalRelations(Entity src, Entities.Resource dst)
         {
             if (src is Entities.impl.Procedure)
