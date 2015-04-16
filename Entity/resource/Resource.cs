b032f9a3b0f356155e2801da39100aac391ab8b5
avfrolov <avfrolov9302@gmail.com>
2015-04-16 23:13:18 +0300

    fix build + class Resource becomes abstract

--- Entity/resource/Resource.cs
+++ Entity/resource/Resource.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    // TODO make abstract! remove efficiency and price
-    public class Resource
+    public abstract class Resource
     {
 
         public ResourceType type { get; set; }
