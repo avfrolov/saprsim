ee03f398546cea48c4812cb9a6e4b2e303bb3cb8
avfrolov <avfrolov9302@gmail.com>
2015-04-18 00:52:41 +0300

    cyclic subprocess dependency checker

--- sapr-sim/Transformers/SubdiagramTransformer.cs
+++ sapr-sim/Transformers/SubdiagramTransformer.cs
@@ -14,12 +14,20 @@ namespace EntityTransformator.Transformers
     public class SubdiagramTransformer : Transformer
     {
 
+        private static bool cyclicDependencyChecker = false;
+
         public Entities.Entity transform(UIEntity entity)
         {
+            if (cyclicDependencyChecker) return null;
+
             TransformerService ts = new TransformerService();
             SubDiagram sd = entity as SubDiagram;
             Submodel submodel = new Submodel() { name = entity.EntityName, id = entity.Id };
+
+            cyclicDependencyChecker = true;
             submodel.setEntites(ts.transform(new FileService().open(sd.ProjectItem.FullPath).Children));
+            cyclicDependencyChecker = false;
+            
             submodel.setResources(ts.getResources());
             return submodel;
         }
