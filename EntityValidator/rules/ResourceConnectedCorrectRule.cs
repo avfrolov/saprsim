b03ec388f27dc48749d2f2b7de3746baee6f046f
avfrolov <avfrolov9302@gmail.com>
2015-04-18 00:11:37 +0300

    new resource connection rule

--- EntityValidator/rules/ResourceConnectedCorrectRule.cs
+++ EntityValidator/rules/ResourceConnectedCorrectRule.cs
@@ -12,7 +12,12 @@ namespace EntityValidator.rules
     public class ResourceConnectedCorrectRule : Rule
     {
 
+        private List<Identifable> failed = new List<Identifable>();
         private List<Resource> resources = Model.Instance.getResources();
+
+        Dictionary<WorkerResource, int> workers = new Dictionary<WorkerResource, int>();
+        Dictionary<InstrumentResource, int> instruments = new Dictionary<InstrumentResource, int>();
+        Dictionary<MaterialResource, int> materials = new Dictionary<MaterialResource, int>();
 
         public ResourceConnectedCorrectRule(List<Entity> entities, List<Resource> resources) : base(entities)
         {
@@ -21,29 +26,87 @@ namespace EntityValidator.rules
 
         public override bool validate()
         {
-            int procedureResources = 0;
+            foreach (Resource res in resources)
+            {
+                if (ResourceType.WORKER.Equals(res.type))
+                    workers.Add(res as WorkerResource, res.count);
+                else if (ResourceType.INSTRUMENT.Equals(res.type))
+                    instruments.Add(res as InstrumentResource, res.count);
+                else if (ResourceType.MATERIAL.Equals(res.type))
+                    materials.Add(res as MaterialResource, res.count);
+            }
+
             foreach (Entity e in entities)
             {
                 if (e is Procedure)
                 {
                     Procedure p = (e as Procedure);
-                    foreach(Resource res in p.getResources())
-                        procedureResources += res.totalCount;
+                    foreach (Resource res in p.getResources())
+                    {
+                        if (ResourceType.WORKER.Equals(res.type))
+                            processWorkerResources(res as WorkerResource);
+                        else if (ResourceType.INSTRUMENT.Equals(res.type))
+                            processInstrumentResources(new List<InstrumentResource>() { res as InstrumentResource });
+                        else if (ResourceType.MATERIAL.Equals(res.type))
+                            processMaterialResources(new List<MaterialResource>() { res as MaterialResource });
+                    }
                 }
             }
 
-            int totalResources = 0;
-            foreach (Resource res in resources)
-                totalResources += res.count;
+            failed.AddRange(workers.Keys);
+            failed.AddRange(instruments.Keys);
+            failed.AddRange(materials.Keys);
 
-            return procedureResources >= totalResources;
+            return failed.Count == 0;
         }
 
         public override List<ValidationError> explain()
         {
             List<ValidationError> errors = new List<ValidationError>();
-            errors.Add(new ValidationError("Ресурсы не корректно подключены"));            
+            foreach(Identifable fail in failed)
+                errors.Add(new ValidationError("Ресурс '" + fail.name + "' не верно подключен", fail));            
             return errors;
+        }
+
+        private void processWorkerResources(WorkerResource wr)
+        {
+            processInstrumentResources(wr.instruments);
+            processMaterialResources(wr.materials);
+
+            if (workers.ContainsKey(wr))
+            {
+                workers[wr] -= wr.count;
+                if (workers[wr] == 0)
+                    workers.Remove(wr);
+            }
+        }
+
+        private void processInstrumentResources(List<InstrumentResource> resources)
+        {
+            foreach (InstrumentResource ir in resources)
+            {
+                processMaterialResources(ir.materials);
+
+                if (instruments.ContainsKey(ir))
+                {
+                    instruments[ir] -= ir.count;
+                    if (instruments[ir] == 0)
+                        instruments.Remove(ir);
+                }
+            }
+        }
+
+        private void processMaterialResources(List<MaterialResource> resources)
+        {
+            foreach (MaterialResource mr in resources)
+            {
+                if (materials.ContainsKey(mr))
+                {
+                    materials[mr] -= mr.count;
+                    if (materials[mr] == 0)
+                        materials.Remove(mr);
+                }
+            }
         }
     }
 }
